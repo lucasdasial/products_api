31a5d91a228371cba734d1063f6ebbf9bded160f
Lucas Alves <as.lucasalves@gmail.com>
2024-08-03 12:31:36 -0400

    update models

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,19 @@
-﻿namespace products_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace products_api.Models
 {
     public class Category
     {
         public int Id { get; set; }
-        public String? Name{ get; set; }
-        public String? ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MaxLength(80)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MaxLength(300)]
+        public string ImageUrl { get; set; } = string.Empty;
         public ICollection<Product> Products { get; set; } = [];
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,15 +1,28 @@
-﻿namespace products_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace products_api.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public String? Name { get; set; }
-        public String? ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MaxLength(80)]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MaxLength(300)]
+        public string? ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
         public decimal Price { get; set; }
-        public float QuantityInStock { get; set; }
+        public float QuantityInStock { get; set; } = 1F;
         public DateTime CreateAt { get; set; } = DateTime.Now;
 
         public int CategoryId { get; set; }
-        public Category Category { get; set; } = new();
+
+        [JsonIgnore]
+        public Category? Category { get; set; }
     }
 }
