e294313f32ce1f5ed6208ceef8e80e3085ef8e09
Lucas Alves <as.lucasalves@gmail.com>
2024-08-03 10:52:42 -0400

    Adicionar arquivos de projeto.

---
+++ Models/Catetegory.cs
@@ -0,0 +1,9 @@
+﻿namespace products_api.Models
+{
+    public class Catetegory
+    {
+        public int Id { get; set; }
+        public String? Name{ get; set; }
+        public String? ImageUrl { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace products_api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String? Name { get; set; }
+        public String? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public float QuantityInStock { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
