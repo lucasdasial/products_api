8564798e9a1957796403cc1a7cfb5036bd32b843
Lucas Alves <as.lucasalves@gmail.com>
2024-08-04 17:03:07 -0400

    add migration to update tables

---
+++ Migrations/20240804205930_TestUpdateTabbles.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace products_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestUpdateTabbles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Product",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Category",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Product",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Category",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 300);
+        }
+    }
+}
