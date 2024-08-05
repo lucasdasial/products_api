4e1f7714ed591e9e721d8b26ff22ca050b19eaa6
Lucas Alves <as.lucasalves@gmail.com>
2024-08-05 15:09:52 -0400

    chore: add seeding to product and categories

---
+++ Migrations/20240805171605_SeedingCategories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace products_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert  into Category(Name, ImageUrl) values('Bebidas', 'bebidas.png')");
+            mb.Sql("Insert  into Category(Name, ImageUrl) values('Lanches', 'lanches.png')");
+            mb.Sql("Insert  into Category(Name, ImageUrl) values('Sobremesa', 'sobremesa.png')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Category");
+        }
+    }
+}
---
+++ Migrations/20240805183250_SeedingProducts.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace products_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert  into Product(Name, ImageUrl, Price, QuantityInStock, CreateAt, CategoryId)" +
+                " values('Coca Diet', 'coca.png', 3.99, 10, datetime('now'), 1)");
+            mb.Sql("Insert  into Product(Name, ImageUrl, Price, QuantityInStock, CreateAt, CategoryId)" +
+                " values('Coxinaha', 'coxinha.png', 4.99, 10, datetime('now'), 2)");
+            mb.Sql("Insert  into Product(Name, ImageUrl, Price, QuantityInStock, CreateAt, CategoryId)" +
+                " values('Sorvete de morango', 'sorvete.png', '13.99', 10, datetime('now'), 3)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+             mb.Sql("Delete from Product");
+        }
+    }
+}
